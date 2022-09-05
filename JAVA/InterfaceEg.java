3229174c6597876c47b77a768e321cc94f0a3a3b
235749-Vishnu-S-Nath <112605510+235749-Vishnu-S-Nath@users.noreply.github.com>
2022-09-05 17:14:16 +0530

    Add files via upload

---
+++ JAVA/InterfaceEg.java
@@ -0,0 +1,18 @@
+package com.ust;
+
+interface Vehicle{
+	public abstract String getColour();
+	public abstract String getEngineType();
+	public static final int x=4;
+}
+
+abstract public class InterfaceEg implements Vehicle {
+	public String getColour()
+	{
+		return "Green";
+	}
+/**	public String getEngineType() {
+		return "Manual";
+	}
+	*/
+}
