b8f83331d24cb3abfcc4fe8b025136d321c437af
235749-Vishnu-S-Nath <112605510+235749-Vishnu-S-Nath@users.noreply.github.com>
2022-10-26 11:03:40 +0530

    Create user.java

---
+++ Coding Practice/user.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.lang.*;
+
+class User{
+    private int id;
+    private String username;
+    private String password;
+    
+    User(int id,String username,String password){
+        this.id=id;
+        this.username=username;
+        this.password=password;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+}
+
+class UserBO{
+    
+}
+
+class Main{
+    
+}
