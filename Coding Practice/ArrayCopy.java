3e67a6b5e9641101b4afe203aa31941e4558a615
235749-Vishnu-S-Nath <112605510+235749-Vishnu-S-Nath@users.noreply.github.com>
2022-10-27 10:18:16 +0530

    Create ArrayCopy.java

---
+++ Coding Practice/ArrayCopy.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+import java.lang.*;
+class Main {
+    public static void main(String[] args) {
+        int A[]={1,2,3};
+        int n=3;
+        int B[]=new int[n];
+        for(int i=0;i<n;i++){
+            B[i]=A[i];
+            System.out.println(B[i]);
+        }
+        
+    }
+}
