e8749ae47f179677d9059177980863591c0baa86
235749-Vishnu-S-Nath <112605510+235749-Vishnu-S-Nath@users.noreply.github.com>
2022-10-27 10:11:17 +0530

    Create parse.java

---
+++ Coding Practice/parse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+import java.lang.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        String str="9999";
+        long varLong = Long.parseLong(str);
+        int varInt = Integer.parseInt(str);
+        System.out.println(varLong);
+        System.out.println(varInt);
+    }
+}
